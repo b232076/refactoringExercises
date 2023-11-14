5e94f2221749f15302675f0bf938e1c9e7fa4d71
Beatriz Moura <141643695+b232076@users.noreply.github.com>
2023-11-14 10:29:08 -0300

    Update Basket.java
    Comentário

--- src/exercise5/Basket.java
+++ src/exercise5/Basket.java
@@ -3,7 +3,7 @@ package exercise5;
 public class Basket {
 
 	// ..
-	float getTotalPrice(Item i) {
+	float getTotalPrice(Item i) { //O parâmetro é apenas um item, sendo característica apenas da classe item 
 		float price = i.getPrice() + i.getTax();
 		if (i.isOnSale())
 			price = price - i.getSaleDiscount() * price;
